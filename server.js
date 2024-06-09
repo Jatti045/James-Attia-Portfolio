84efe73c352f31e9d8d196e17a74e7e41870bf8d
James <Jatti045@uottawa.ca>
2024-06-09 19:58:00 -0400

    git add . git commit -m "Fixed bug: Email sending status displayed correctly"

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: ".git/.env" });
 const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
