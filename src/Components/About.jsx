e9c43e2666c8dfe3f706f3be052074acd206abfd
James <Jatti045@uottawa.ca>
2024-12-28 17:36:24 -0500

    updated projects section.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,23 +8,32 @@ const About = () => {
         <p className={styles.sectionText}>
           I am a dedicated software engineering student at the University of
           Ottawa with a strong passion for coding and problem-solving. My
-          experience spans both frontend and backend development, where I thrive
-          on creating elegant solutions to complex challenges.
+          academic journey has provided me with a solid foundation in both
+          frontend and backend development, and I enjoy creating elegant
+          solutions to complex challenges.
         </p>
         <p className={styles.sectionText}>
-          I have hands-on experience developing a Business Management System in
-          Java, utilizing object-oriented principles and JSON-based data
-          persistence to manage customer data efficiently. Additionally, I
+          My hands-on experience includes developing a Business Management
+          System in Java, utilizing object-oriented principles and JSON-based
+          data persistence to efficiently manage customer data. Additionally, I
           designed and implemented a Pokémon search app using HTML, CSS, and
-          JavaScript, demonstrating my skills in frontend development and API
-          integration.
+          JavaScript, showcasing my frontend development skills and API
+          integration expertise.
         </p>
         <p className={styles.sectionText}>
-          Through these projects, I have honed not only my technical abilities
-          but also my communication and teamwork skills. Proficient in Java,
-          Python, HTML/CSS, and JavaScript, and adept with tools like VS Code
-          IntelliJ IDEA, and PyCharn, I am committed to delivering innovative
-          and efficient solutions that meet real-world needs.
+          I've also worked on projects such as a Learning Management System
+          Accessibility Tool built with React and Firebase, designed to help
+          professors enhance accessibility in course materials, and an Event
+          Attendance Management System (EAMS) for universities, leveraging
+          Android, Java, and Firebase for seamless event management.
+        </p>
+        <p className={styles.sectionText}>
+          With proficiency in Java, Python, JavaScript, HTML/CSS, and ReactJS,
+          as well as experience with developer tools like VS Code, IntelliJ
+          IDEA, and PyCharm, I am committed to delivering innovative and
+          efficient solutions that meet real-world needs. I thrive in
+          collaborative environments, bringing strong communication and teamwork
+          skills to every project.
         </p>
       </section>
     </>
